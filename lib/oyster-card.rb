1ef9ec84b8491504f193523ae1d08022e619bb89
ollie beney <ombeney@gmail.com>
2020-09-07 16:38:08 +0100

    added balance change on touch out/touchin

--- lib/oyster-card.rb
+++ lib/oyster-card.rb
@@ -1,14 +1,35 @@
 class Oystercard 
   MAX_BALANCE = 90
-  attr_reader :balance
+  MINIMUM_FARE = 1
+  attr_accessor :balance, :travelling
   
   def initialize(balance=0)
     @balance = 0
+    @travelling = false 
+    
   end
 
   def top_up(amount)
-    @balance += amount
+    fail "The maximum is #{MAX_BALANCE}" if (@balance + amount) > MAX_BALANCE
+    @balance += amount 
+  end
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
+  def in_journey?
+    @travelling
+  end
+  
+  def touch_in
+    raise "Insufficient funds" if @balance < 1
+    @travelling = true 
+  end
+
+  def touch_out
+      @travelling = false
+      @balance -= MINIMUM_FARE
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -18,7 +18,57 @@ describe Oystercard do
     card.top_up(5)
     expect(card.balance).to eq 5
   end
+
   it 'will not let the @balance be more than MAX_BALANCE' do
     expect { subject.top_up(100) }.to raise_error "The maximum is #{Oystercard::MAX_BALANCE}"
   end
+
+  it 'has a deduct method' do
+    expect(subject).to respond_to(:deduct).with(1).argument
+  end
+
+  it "deducts the fare from the balance" do
+    subject.top_up(10)
+    expect(subject.deduct(5)).to eq 5
+  end
+
+  it "has an 'in_journey?' method" do
+    expect(subject).to respond_to(:in_journey?)
+  end
+
+  it "has a touch_in method" do
+    expect(subject).to respond_to(:touch_in)
+  end
+
+  it "it has a touch_out method" do
+    expect(subject).to respond_to(:touch_out)
+  end
+
+  it "in_journey returns a boolean" do
+    expect(subject.in_journey?).to eq(true).or eq false
+  end
+
+  it "sets @travelling to true when touch in is called" do
+    subject.balance = 50
+    subject.touch_in
+    expect(subject.in_journey?).to eq true
+  end
+
+  it "sets @travelling to false when touch_out is called" do
+    subject.balance = 2
+    subject.touch_in
+    subject.touch_out
+    expect(subject.in_journey?).to eq false
+  end
+
+  it "raises insufficient funds error if balance is below 1 when touching in" do
+    expect {subject.touch_in}.to raise_error "Insufficient funds"
+  end
+
+  it "deducts the minimum fare from balance on touch_out" do
+    subject.balance = 5
+    subject.touch_in
+    expect {subject.touch_out}. to change{subject.balance}.by((Oystercard::MINIMUM_FARE)/-1)
+  end
+
 end
