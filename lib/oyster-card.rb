341955187c4952a70471fdd6e993cfe95c7b0939
Richard <rich_pryce@yahoo.co.uk>
2020-09-07 15:35:40 +0100

    balance tops up. Maxbalance set

--- lib/oyster-card.rb
+++ lib/oyster-card.rb
@@ -1,8 +1,14 @@
 class Oystercard 
+  MAX_BALANCE = 90
   attr_reader :balance
   
   def initialize(balance=0)
     @balance = 0
+  end
+
+  def top_up(amount)
+    @balance += amount
+
   end
 
 end
--- spec/oyster_spec.rb
+++ spec/oyster_spec.rb
@@ -8,4 +8,17 @@ describe Oystercard do
   it 'has a balance' do
     expect(subject).to respond_to(:balance)
   end
+
+  it 'has a top_up method that responds to 1 argument' do
+    expect(subject).to respond_to(:top_up).with(1).argument
+  end
+
+  it 'will increase @balance by 5 when topped up' do
+    card = Oystercard.new
+    card.top_up(5)
+    expect(card.balance).to eq 5
+  end
+  it 'will not let the @balance be more than MAX_BALANCE' do
+    expect { subject.top_up(100) }.to raise_error "The maximum is #{Oystercard::MAX_BALANCE}"
+  end
 end
